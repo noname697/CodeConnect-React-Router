4664426a2121c8981fb0fc21d45233a989c4c965
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-08 13:42:27 -0300

    Implementando login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -14,8 +14,26 @@ import { TextDivider } from "../../components/TextDivider";
 import { Providers } from "../../components/Providers";
 import { Link } from "../../components/Link";
 import styles from "./login.module.css";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router";
 
 export const Login = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const onSubmit = (formData) => {
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const response = login(email, password);
+
+    if (response.success) {
+      navigate("/");
+    } else {
+      console.error(response.error);
+    }
+  };
+
   return (
     <AuthLayout>
       <AuthFormContainer bannerSrc={banner}>
@@ -25,7 +43,7 @@ export const Login = () => {
         <Typography variant="h2" color="--offwhite">
           Boas-vindas! Faça seu login.
         </Typography>
-        <Form action="">
+        <Form action={onSubmit}>
           <Fieldset>
             <Label>E-mail</Label>
             <Input
