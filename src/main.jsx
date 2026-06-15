f3087237d071ed3190ef8860b9950add325642f9
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-15 13:02:20 -0300

    Criando segmentos dinamicos na URL

--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,7 @@ createRoot(document.getElementById("root")).render(
             }
           />
           <Route
-            path="blog-post"
+            path="blog-post/:slug"
             element={
               <ProtectedRoute>
                 <BlogPost />
