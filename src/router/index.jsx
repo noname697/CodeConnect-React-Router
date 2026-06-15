54cd719b47864fd41c2b002da0576f5fe51b285a
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-15 13:05:20 -0300

    Organizando rotas

---
+++ src/router/index.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Register } from "../pages/Register";
+import { Logout } from "../pages/Logout";
+import { Login } from "../pages/Login";
+import { ProtectedRoute } from "../components/ProtectedRoute";
+import { Feed } from "../pages/Feed";
+import { BlogPost } from "../pages/BlogPost";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth">
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="logout" element={<Logout />} />
+        </Route>
+
+        <Route path="/">
+          <Route
+            path=""
+            element={
+              <ProtectedRoute>
+                <Feed />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="blog-post/:slug"
+            element={
+              <ProtectedRoute>
+                <BlogPost />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
